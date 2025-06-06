7378d4ed3576532bef3464eadc13460e43356304
Prajjwal Sahu <prajjwal2051@gmail.com>
2025-06-06 19:22:25 +0530

    set server limit and code to accept the new clients

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ import threading    # handels ,multiple clinet operations at one time
 # now i will provide the server host and port for the communication
 HOST='127.0.0.1'    # this means our own local machine is used as a server for the communication
 PORT=57815  # we can use any port for our use cases
+LISTENER_LIMIT=5
 
 # main fucntion
 def main():
@@ -30,7 +31,19 @@ def main():
     except:
         print(f"Unable to bind with the host {HOST} and port {PORT}")
 
+    '''
+    now we will set the server limit this is necessary so that more load doesn't come to our 
+    computer so we will set the limit how many clinet can connect to our server at a time 
+    '''
+    server.listen(LISTENER_LIMIT)
 
+    ''' now we will create a while loop that will continously listen to clinet connections'''
+
+    while True:
+        client,address=server.accept()  # we created two varaibles that contain the clinet socket using and what is the address of it
+        print(f"Sucessfully connected to client {address[0]} {address[1]}") 
+        # here address is the tuple so address[0] will print the host and address[1] will print the port which is using
+        
 
 
 if __name__=="__main__":
